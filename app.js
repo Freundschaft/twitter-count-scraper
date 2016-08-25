c843fcd0036ebe407a83997250c335a7d7a1338b
Qiong Wu <qiong.wu@gfnork.de>
2016-08-26 00:19:02 +0200

    now gets counts from json object instead of display divs

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var osmosis = require('osmosis'),
   resonatePassword = '';
 
 var ws_name = "twitter Results";
-var attributes = ['username', 'followers', 'following', 'tweets', 'favorites', 'all_lists'];
+var attributes = ['username', 'followers_count', 'friends_count', 'statuses_count', 'favourites_count', 'listed_count'];
 
 function Workbook() {
   if (!(this instanceof Workbook)) return new Workbook();
@@ -57,30 +57,10 @@ var getTwitterInfo = function (twitterUsername) {
     var twitterInfo = {username: twitterUsername};
     osmosis
       .get('twitter.com/' + twitterUsername)
-      .find("a[@data-nav='followers']/span.ProfileNav-value")
-      .set('followers')
+      .find("#init-data/@value")
+      .set('jsonData')
       .data(function (result) {
-        twitterInfo = R.merge(twitterInfo, result);
-      })
-      .find("a[@data-nav='following']/span.ProfileNav-value")
-      .set('following')
-      .data(function (result) {
-        twitterInfo = R.merge(twitterInfo, result);
-      })
-      .find("a[@data-nav='tweets']/span.ProfileNav-value")
-      .set('tweets')
-      .data(function (result) {
-        twitterInfo = R.merge(twitterInfo, result);
-      })
-      .find("a[@data-nav='favorites']/span.ProfileNav-value")
-      .set('favorites')
-      .data(function (result) {
-        twitterInfo = R.merge(twitterInfo, result);
-      })
-      .find("a[@data-nav='all_lists']/span.ProfileNav-value")
-      .set('all_lists')
-      .data(function (result) {
-        twitterInfo = R.merge(twitterInfo, result);
+        twitterInfo = R.merge(twitterInfo, JSON.parse(result.jsonData).profile_user);
       })
       .done(function () {
         resolve(twitterInfo);
@@ -94,39 +74,45 @@ var getTwitterInfo = function (twitterUsername) {
 getUserDirectory()
   .then(function (twitterUsernames) {
     var getAllTwitterInfos = Promise.resolve(twitterUsernames).map(getTwitterInfo, {concurrency: 1});
-    getAllTwitterInfos
-      .then(function (results) {
-        R.addIndex(R.forEach)(function (heading, headingIndex) {
-          var cell_ref = XLSX.utils.encode_cell({c: headingIndex, r: 0});
-          ws[cell_ref] = {t: "s", v: heading};
-          range.e.c = headingIndex;
-        }, attributes);
-        R.addIndex(R.forEach)(function (twitterInfo, index) {
-          R.addIndex(R.forEach)(function (key, keyIndex) {
-            var cell_ref = XLSX.utils.encode_cell({c: keyIndex, r: index + 1});
-            if (isNaN(twitterInfo[key])) {
-              ws[cell_ref] = {t: "s", v: twitterInfo[key]};
-            } else {
-              ws[cell_ref] = {t: "n", v: twitterInfo[key]};
-            }
-            range.e.r = index + 1;
-          }, attributes);
-        }, results);
+    return getAllTwitterInfos;
+  })
+  .then(function (results) {
+    console.log('processing results...');
+    R.addIndex(R.forEach)(function (heading, headingIndex) {
+      var cell_ref = XLSX.utils.encode_cell({c: headingIndex, r: 0});
+      ws[cell_ref] = {t: "s", v: heading};
+      range.e.c = headingIndex;
+    }, attributes);
+    R.addIndex(R.forEach)(function (twitterInfo, index) {
+      R.addIndex(R.forEach)(function (key, keyIndex) {
+        var cell_ref = XLSX.utils.encode_cell({c: keyIndex, r: index + 1});
+        if (isNaN(twitterInfo[key])) {
+          ws[cell_ref] = {t: "s", v: twitterInfo[key]};
+        } else {
+          ws[cell_ref] = {t: "n", v: twitterInfo[key]};
+        }
+        range.e.r = index + 1;
+      }, attributes);
+    }, results);
 
-        ws['!ref'] = XLSX.utils.encode_range(range);
+    ws['!ref'] = XLSX.utils.encode_range(range);
 
-        /* add worksheet to workbook */
-        wb.SheetNames.push(ws_name);
-        wb.Sheets[ws_name] = ws;
-        /* write file */
-        XLSX.writeFile(wb, 'results.xlsx');
-      });
-  });
-return;
+    /* add worksheet to workbook */
+    wb.SheetNames.push(ws_name);
+    wb.Sheets[ws_name] = ws;
+    /* write file */
+    console.log('writing results...');
+    XLSX.writeFile(wb, 'results.xlsx');
+    console.log('done.');
+  })
+  .catch(console.log);
 
-var params = {screen_name: 'resonatecoop'};
-client.get('followers/ids', params, function (error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
-  }
-});
+/*
+
+ var params = {screen_name: 'resonatecoop'};
+ client.get('followers/ids', params, function (error, tweets, response) {
+ if (!error) {
+ console.log(tweets);
+ }
+ });
+ */
