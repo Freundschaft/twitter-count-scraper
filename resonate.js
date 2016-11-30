52b5ed8f39dd9b5e1ad6b8c5c562558a2b0de4f3
Qiong Wu <qiong.wu@gfnork.de>
2016-11-30 03:01:08 +0100

    disable mock

--- resonate.js
+++ resonate.js
@@ -176,28 +176,28 @@ console.time('script execution duration');
 getUserDirectory()
   .bind({})
   .then(function (resonateProfiles) {
-    resonateProfiles = [{
-      "ID": "72",
-      "user_login": "mattblack",
-      "user_nicename": "mattblack",
-      "user_email": "mattb@ninjatune.net",
-      "user_url": "",
-      "user_registered": "2016-07-14 19:12:47",
-      "display_name": "Matt Black",
-      "spam": "0",
-      "deleted": "0",
-      "roles": [
-        "subscriber"
-      ],
-      "first_name": "Matt",
-      "last_name": "Black",
-      "community_role": "member",
-      "account_status": "approved",
-      "profile_pic_original": "https:\/\/resonate.is\/wp-content\/uploads\/ultimatemember\/544\/profile_photo.jpg?1480468003",
-      "profile_pic_normal": "https:\/\/resonate.is\/wp-content\/uploads\/ultimatemember\/544\/profile_photo-300.jpg?1480468003",
-      "profile_pic_small": "https:\/\/resonate.is\/wp-content\/uploads\/ultimatemember\/544\/profile_photo-40.jpg?1480468003",
-      "cover_photo": ""
-    }];
+    // resonateProfiles = [{
+    //   "ID": "72",
+    //   "user_login": "mattblack",
+    //   "user_nicename": "mattblack",
+    //   "user_email": "mattb@ninjatune.net",
+    //   "user_url": "",
+    //   "user_registered": "2016-07-14 19:12:47",
+    //   "display_name": "Matt Black",
+    //   "spam": "0",
+    //   "deleted": "0",
+    //   "roles": [
+    //     "subscriber"
+    //   ],
+    //   "first_name": "Matt",
+    //   "last_name": "Black",
+    //   "community_role": "member",
+    //   "account_status": "approved",
+    //   "profile_pic_original": "https:\/\/resonate.is\/wp-content\/uploads\/ultimatemember\/544\/profile_photo.jpg?1480468003",
+    //   "profile_pic_normal": "https:\/\/resonate.is\/wp-content\/uploads\/ultimatemember\/544\/profile_photo-300.jpg?1480468003",
+    //   "profile_pic_small": "https:\/\/resonate.is\/wp-content\/uploads\/ultimatemember\/544\/profile_photo-40.jpg?1480468003",
+    //   "cover_photo": ""
+    // }];
     console.log('start retrieving profile information for users...');
     return Promise.resolve(resonateProfiles)
       .map(getAllInfo, {concurrency: 1});
